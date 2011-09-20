26ed19de678bcb721c66a925a4f9943b60ca33ca
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-21 02:01:30 +0800

    Refactored views & rake tasks

--- lib/tasks/tags.rb
+++ lib/tasks/tags.rb
@@ -1,106 +1,69 @@
+def load_jekyll
+  require 'jekyll'
+  include Jekyll::Filters
+  @options = Jekyll.configuration({})
+  @site = Jekyll::Site.new(@options)
+  @site.read_posts('')
+end
+
 namespace :tags do
-  desc "Generate a basic tag cloud"
-  task :cloud_basic do
-      puts 'Generating tag cloud...'
-      require 'rubygems'
-      require 'jekyll'
-      include Jekyll::Filters
-      options = Jekyll.configuration({})
-      site = Jekyll::Site.new(options)
-      site.read_posts('')
-
-      html = ''
-      site.tags.sort.each do |tag, posts|
-        s = posts.count
-        font_size = 12 + (s*1.2);
-        html << "<a href=\"#{options["url"]}/tag/#{tag}/\" title=\"Pages tagged #{tag}\" style=\"font-size: #{font_size}px; line-height:#{font_size}px\" rel=\"tag\">#{tag}</a> "
-      end
-
-      FileUtils.mkdir_p "tags"
-      File.open('_includes/tags.html', 'w+') do |file|
-        file.puts html
-      end
-      puts 'Done.'
-    end
-
-  desc "Generate a full tag cloud"
+  desc "Generate a tag cloud for sidebar"
   task :cloud do
-      puts 'Generating tag cloud...'
-      require 'rubygems'
-      require 'jekyll'
-      include Jekyll::Filters
+    load_jekyll
 
-      options = Jekyll.configuration({})
-      site = Jekyll::Site.new(options)
-      site.read_posts('')
-
-      html =<<-HTML
----
-layout: default
-title: Tags
-type: A tag cloud
----
-
-<h1>Tag cloud for {{site.title}}</h1>
-
-<p>Click on a tag to see the relevant posts.</p>
-HTML
-
-    site.tags.sort.each do |tag, posts|
-      html << <<-HTML
-      HTML
-
+    puts 'Generating tag cloud...'
+    html = ''
+    @site.tags.sort.each do |tag, posts|
       s = posts.count
-      font_size = 12 + (s*1.5);
-      html << "<a href=\"#{options["url"]}/tag/#{tag}/\" title=\"Entries tagged #{tag}\" style=\"font-size: #{font_size}px; line-height:#{font_size}px\">#{tag}</a> "
+      font_size = 12 + (s*1.2);
+      html << "<a href=\"#{@options["url"]}/tag/#{tag}/\" title=\"Pages tagged #{tag}\" style=\"font-size: #{font_size}px; line-height:#{font_size}px\" rel=\"tag\">#{tag}</a>\n"
     end
-
-    html << "<p>You may also wish to browse the <a href=\"#{options["url"]}/archives/\" title=\"Archives for {{site.title}}\">archives</a>."
 
     FileUtils.mkdir_p "tags"
-    File.open('tags/index.html', 'w+') do |file|
+    File.open('_includes/tags.html', 'w+') do |file|
       file.puts html
     end
-
     puts 'Done.'
+  end
+
+  task :index do
+
   end
 end
 
-desc 'Generate tags page'
+
+desc 'Generate tags pages'
 task :tags do
+  load_jekyll
+
   puts "Generating tags..."
-  require 'rubygems'
-  require 'jekyll'
-  include Jekyll::Filters
 
-  options = Jekyll.configuration({})
-  site = Jekyll::Site.new(options)
-  site.read_posts('')
-  site.tags.sort.each do |raw_tag, posts|
-    tag = raw_tag.gsub(',', '').strip
+  # Remove tags directory before regenerating
+  FileUtils.rm_rf("tags")
 
-    html = ''
-    html << <<-HTML
+  @site.tags.sort.each do |tag, posts|
+    html = <<-HTML
 ---
 layout: default
-title: Entries tagged "#{tag}"
-type: "#{tag.gsub(/\b\w/){$&.upcase}}"
+title: "tagged: #{tag}"
 ---
-    <h1 id="#{tag}">Entries tagged "#{tag}"</h1>
-    <a href="#{options["url"]}/tags/" title="Tag cloud for {{site.title}}">&laquo; Show all tags...</a>
-    HTML
+<div id="content">
+  <div class="posts-list">
+    {% for post in site.posts %}
+      {% for tag in post.tags %}
+        {% if tag == "#{tag}" %}
+          {% include post_excerpt.html %}
+        {% endif %}
+      {% endfor %}
+    {% endfor %}
+  </div>
+</div>
 
-    html << '<ul class="posting_list">'
-    posts.each do |post|
-      post_data = post.to_liquid
-      html << <<-HTML
-        <li><a href="#{options["url"]}/#{post.url}" rel="tag" title="Entries tagged #{post_data['title']}">#{post_data['title']}</a></li>
-      HTML
-    end
-    html << '</ul>'
+{% include default_sidebar.html %}
 
-    html << '<p>You may also be interested in browsing the <a href="#'+options["url"]+'/archives/" title="Archives for {{site.title}}">archives</a> or seeing <a href="'+options["url"]+'/tags/" title="Tag cloud for {{site.title}}">the tag cloud for {{site.title}}</a>.'
-    FileUtils.mkdir_p "tags/#{tag}"
+HTML
+
+    FileUtils.mkdir_p("tags/#{tag}")
     File.open("tags/#{tag}/index.html", 'w+') do |file|
       file.puts html
     end
