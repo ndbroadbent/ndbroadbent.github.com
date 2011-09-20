7e98c58f517df67e4f78baebd784eba40e163e22
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-21 04:00:05 +0800

    Added Tags watcher to main watch task

--- lib/tasks/watchers.rb
+++ lib/tasks/watchers.rb
@@ -1,12 +1,11 @@
 desc "Execute all watchers in one process"
 task :watch do
   require 'watcher'
-  watchers = [Watcher::Haml, Watcher::Jammit, Watcher::Image, Watcher::Asset]
+  watchers = [Watcher::Haml, Watcher::Jammit, Watcher::Image, Watcher::Asset, Watcher::Tags]
   watchers.each_with_index do |watcher, i|
     i < (watchers.size - 1) ? Thread.new { watcher.watch; }.run : watcher.watch
     sleep 0.05
   end
-  while true; end
 end
 
 namespace :haml do
